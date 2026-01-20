b53702c83bd3b85031e4a645516088ace3e82b4f
George <101523970+cryptologicpsy@users.noreply.github.com>
2026-01-20 05:08:58 +0200

    Enable CORS for ProxyController
    Add CrossOrigin annotation to allow requests from a specific origin.

--- src/main/java/com/example/crypto/controllers/ProxyController.java
+++ src/main/java/com/example/crypto/controllers/ProxyController.java
@@ -1,3 +1,21 @@
+package com.example.crypto.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
 @RestController
 @RequestMapping("/api/proxy")
 @CrossOrigin(origins = "https://chatapp.georgetzifkas.me")
